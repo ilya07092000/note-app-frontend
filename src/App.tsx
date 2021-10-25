df5abc637f786b394504588565c92bee3068e694
ilya07092000 <ischenko2012@gmail.com>
2021-10-25 22:40:06 +0300

    feat: dashboard comonent with route

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,13 @@
 import React, { FC } from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Dashboard from './pages/dashboard';
+
 import './assets/main.scss';
 
-const App: FC = () => <h1>Note App</h1>;
+const App: FC = () => (
+  <Switch>
+    <Route exact path='/' component={Dashboard}/>
+  </Switch>
+);
 
 export default App;
