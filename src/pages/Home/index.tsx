de97231e30fe023afbc463a38eb1846f2451e12e
ilya07092000 <ischenko2012@gmail.com>
2021-10-27 22:44:48 +0300

    feat: add home page

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,10 @@
+import React, { FC } from 'react';
+import Dashboard from '../../containers/dashboard';
+
+const Home: FC = () => (
+  <Dashboard>
+    <h1>Home</h1>
+  </Dashboard>
+);
+
+export default Home;
