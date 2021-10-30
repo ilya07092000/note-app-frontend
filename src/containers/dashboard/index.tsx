b999302b4d792b7e6d0360529d1354095097277c
ilya07092000 <ischenko2012@gmail.com>
2021-10-30 16:03:16 +0300

    feat: added loader to dashboard

--- src/containers/dashboard/index.tsx
+++ src/containers/dashboard/index.tsx
@@ -1,21 +1,31 @@
 import React, { FC } from 'react';
+import Loader from '../../common/components/Loader';
 import Header from '../../common/components/Header';
 import Sidebar from '../../common/components/Sidebar/Sidebar';
 
 import styles from './styles.module.scss';
 
 export type IDashboardProps = {
-  children: any
+  children: any;
+  isLoading?: boolean;
 };
 
-const Dashboard: FC<IDashboardProps> = ({ children }) => (
+const Dashboard: FC<IDashboardProps> = ({ children, isLoading = false }) => (
   <div className={styles.page}>
     <Header />
     <main className={styles.main}>
       <div className={styles.mainInner}>
         <Sidebar />
         <div className={styles.content}>
-          {children}
+          {isLoading ? (
+            <div className={styles.loaderWrapper}>
+              <div className={styles.loaderInner}>
+                <Loader visible={isLoading} />
+              </div>
+            </div>
+          ) : (
+            children
+          )}
         </div>
       </div>
     </main>
