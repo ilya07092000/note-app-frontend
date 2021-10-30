ba1b181ba6467ff0e127780f8c7b170f6bd0bb2c
ilya07092000 <ischenko2012@gmail.com>
2021-10-30 16:03:43 +0300

    feat: fetched notes

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,36 @@
 import React, { FC } from 'react';
+import { useQuery, gql } from '@apollo/client';
 import Dashboard from '../../containers/Dashboard';
 
-const Home: FC = () => (
-  <Dashboard>
-    <h1>Home</h1>
-  </Dashboard>
-);
+const GET_NOTES = gql`
+  query noteFeed($cursor: String) {
+    noteFeed(cursor: $cursor) {
+      cursor
+      hasNextPage
+      notes {
+        id
+        createdAt
+        content
+        favoriteCount
+        author {
+          username
+          id
+          avatar
+          email
+        }
+      }
+    }
+  }
+`;
+
+const Home: FC = () => {
+  const { data, loading } = useQuery(GET_NOTES);
+  console.log(data);
+  return (
+    <Dashboard isLoading={loading}>
+      <h1>Home</h1>
+    </Dashboard>
+  );
+};
 
 export default Home;
