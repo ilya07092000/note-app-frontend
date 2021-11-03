9ce617b2f6c0d9b9f4214aaf2e5e0b82843918d7
ilya07092000 <ischenko2012@gmail.com>
2021-11-03 15:36:30 +0200

    feat: load more notes

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,8 @@
-import React, { FC, useEffect } from 'react';
+import React, { FC, useCallback, useEffect, useState } from 'react';
 import { useQuery, gql } from '@apollo/client';
 import Note from '../../common/components/Note/Note';
 import Dashboard from '../../containers/Dashboard';
+import Button from '../../common/components/Button';
 import { INote } from '../../interfaces/INote';
 
 import styles from './styles.module.scss';
@@ -31,26 +32,56 @@ const GET_NOTES = gql`
 `;
 
 const Home: FC = () => {
-  const { data, loading } = useQuery(GET_NOTES);
+  const { data, loading, fetchMore } = useQuery(GET_NOTES);
+  const [isLoading, setIsLoading] = useState<boolean>(loading);
 
-  useEffect(() => {
-    console.log(data?.noteFeed);
-  }, [data]);
+  useEffect(() => setIsLoading(loading), [loading]);
+
+  const getMoreNotes = useCallback(() => {
+    setIsLoading(true);
+    fetchMore({
+      variables: {
+        cursor: data.noteFeed.cursor,
+      },
+      updateQuery: (prevRes, { fetchMoreResult }) => {
+        setIsLoading(false);
+
+        return {
+          noteFeed: {
+            cursor: fetchMoreResult.noteFeed.cursor,
+            hasNextPage: fetchMoreResult.noteFeed.hasNextPage,
+            notes: [
+              ...prevRes.noteFeed.notes,
+              ...fetchMoreResult.noteFeed.notes,
+            ],
+            __typename: 'noteFeed',
+          },
+        };
+      },
+    });
+  }, [isLoading, data, fetchMore]);
 
   return (
-    <Dashboard isLoading={loading}>
-      <div className={styles.notes}>
-        {data?.noteFeed?.notes?.length
-          ? data.noteFeed.notes.map((note: INote) => (
-              <Note
-                key={note.id}
-                text={note.content}
-                author={note.author}
-                date={note.createdAt}
-                id={note.id}
-              />
-          ))
-          : null}
+    <Dashboard isLoading={isLoading}>
+      <div className={styles.notesWrap}>
+        <div className={styles.notes}>
+          {data?.noteFeed?.notes?.length
+            ? data.noteFeed.notes.map((note: INote) => (
+                <Note
+                  key={note.id}
+                  text={note.content}
+                  author={note.author}
+                  date={note.createdAt}
+                  id={note.id}
+                />
+            ))
+            : null}
+        </div>
+        {data?.noteFeed?.hasNextPage ? (
+          <div className={styles.loadMoreWrap} onClick={getMoreNotes} >
+            <Button>Load More</Button>
+          </div>
+        ) : null}
       </div>
     </Dashboard>
   );
