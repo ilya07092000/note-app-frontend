8e55bdd560880f830c1f0c459a22ae49fc42a540
ilya07092000 <ischenko2012@gmail.com>
2021-11-14 17:02:07 +0200

    updated form model interface

--- src/interfaces/FormModel.ts
+++ src/interfaces/FormModel.ts
@@ -3,9 +3,16 @@ export type ModelForm = IModelItem[];
 export interface IModelItem {
   name: string;
   required?: boolean;
+  rules?: Rule[];
+  minLength?: number;
 }
 
 export type FormValidateResult = {
   values: Record<string, string>;
   errors: Record<string, string>
 }
+
+export type Rule = {
+  pattern: RegExp
+  msg: string;
+};
