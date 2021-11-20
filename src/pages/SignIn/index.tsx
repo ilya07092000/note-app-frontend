bff150985a6e17f9b3cff1729d0f5dc970f03ba7
ilya07092000 <ischenko2012@gmail.com>
2021-11-20 15:35:32 +0200

    feat: sign in

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,4 +1,6 @@
-import { FC } from 'react';
+import { FC, useState, useContext, useEffect } from 'react';
+import { useMutation } from '@apollo/client';
+import { useHistory } from 'react-router';
 import EnterBox from '../../common/components/EnterBox';
 import Form from '../../containers/Form/index';
 import Input from '../../common/components/Input/index';
@@ -6,6 +8,9 @@ import PasswordInput from '../../common/components/PasswordInput/index';
 import Button from '../../common/components/Button/index';
 import signinModel from './signinModel';
 import { FormValidateResult } from '../../interfaces/FormModel';
+import { SIGN_IN } from '../../common/graphql/user/signin';
+import { ACTION_TYPES as TYPES } from '../../interfaces/MainContext/IActionTypes';
+import { MainContext } from '../../common/context/main';
 
 import styles from './styles.module.scss';
 
@@ -21,32 +26,51 @@ const links = [
 ];
 
 const Signup: FC = () => {
-  const submitForm = (values: FormValidateResult) => {
-    console.log(values);
+  const [formError, setFormError] = useState('');
+  const { dispatch, state: { isAuth } } = useContext(MainContext);
+  const history = useHistory();
+
+  const [signin, { loading }]: any = useMutation(SIGN_IN, {
+    onCompleted: ({ signin }) => {
+      console.log(signin);
+      if (signin.token && signin.user) {
+        localStorage.setItem('token', signin.token);
+        dispatch({ type: TYPES.LOGIN, payload: { user: signin.user } });
+      }
+    },
+    onError: ({ message }) => setFormError(message),
+  });
+
+  const submitForm = ({ values }: FormValidateResult) => {
+    signin({
+      variables: {
+        email: values.email,
+        password: values.password,
+      },
+    });
   };
+
+  useEffect(() => {
+    if (isAuth) {
+      history.push('/');
+    }
+  }, [isAuth]);
 
   return (
     <EnterBox links={links}>
-      <Form
-        onSubmit={submitForm}
-        name='signin'
-        model={signinModel}
-      >
+      <Form onSubmit={submitForm} name='signin' model={signinModel} errorMsg={formError}>
         {/* require only one children */}
         <>
           <Form.Item classList={[styles.inputWrap]}>
             <Input label='Email' placeholder='example@email.com' name='email' id='email' />
           </Form.Item>
           <Form.Item classList={[styles.inputWrap]}>
-            <PasswordInput
-              label='Passowrd'
-              placeholder='password'
-              name='password'
-              id='password'
-            />
+            <PasswordInput label='Passowrd' placeholder='password' name='password' id='password' />
           </Form.Item>
           <div className={styles.submitWrap}>
-            <Button classList={[styles.submitBtn]}>Signin</Button>
+            <Button classList={styles.submitBtn} isLoading={loading}>
+              Signin
+            </Button>
           </div>
         </>
       </Form>
