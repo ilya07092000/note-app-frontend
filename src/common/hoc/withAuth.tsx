ebe7b8719b72a26052316f26037cd9eb5b172f89
ilya07092000 <ischenko2012@gmail.com>
2021-11-23 22:43:56 +0200

    feat: auth hoc

--- src/common/hoc/withAuth.tsx
+++ src/common/hoc/withAuth.tsx
@@ -3,11 +3,12 @@ import { Redirect } from 'react-router-dom';
 import { MainContext } from '../context/main';
 
 const withAuth = (WrappedComponent: any) =>
-  class extends Component {
+  class extends Component<{}, { token: string }> {
     static contextType = MainContext;
 
+    // eslint-disable-next-line class-methods-use-this
     render() {
-      if (!this.context.state.isAuth) {
+      if (!localStorage.getItem('token')) {
         return <Redirect to='/signup' />;
       }
 
