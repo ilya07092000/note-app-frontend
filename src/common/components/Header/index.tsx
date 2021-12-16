d21b9b9d6f2567f37fbc2232daed1b4c4782f018
ilya07092000 <ischenko2012@gmail.com>
2021-12-16 22:41:14 +0200

    fix: header ui

--- src/common/components/Header/index.tsx
+++ src/common/components/Header/index.tsx
@@ -1,13 +1,17 @@
 import React, { FC } from 'react';
 import combineCss from '../../helpers/combineCss';
+import AccountDropdown from '../../../containers/AccountDropdown';
 
 import styles from './index.module.scss';
 
 const Header: FC = () => (
   <header className={styles.header}>
-    <div className={combineCss(['container', '_sm'])}>
-      <div>
-        <p className={styles.title}>Note-App</p>
+    <div className={combineCss(['container'])}>
+      <div className={styles.headerInner}>
+        <p className={styles.title}>notify</p>
+        <div className={styles.dropdownWrap}>
+          <AccountDropdown />
+        </div>
       </div>
     </div>
   </header>
