a54e5483b750d6557980864d02374b4b904d3f41
ilya07092000 <ischenko2012@gmail.com>
2021-12-18 20:07:51 +0200

    feat: verify token

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
-import React, { FC, lazy, Suspense } from 'react';
+import React, { FC, lazy, Suspense, useContext } from 'react';
+import { useQuery } from '@apollo/client';
 import { Route, Switch } from 'react-router-dom';
 import './assets/styles/main.scss';
 import Signup from './pages/Signup/index';
 import Signin from './pages/SignIn/index';
 import withAuth from './common/hoc/withAuth';
+import { VERIFY_TOKEN } from './common/graphql/user/verifyToken';
+import { MainContext } from './common/context/main';
+import { ACTION_TYPES as TYPES } from './interfaces/MainContext/IActionTypes';
 
 const Home = lazy(() => import('./pages/Home/index'));
 const Note = lazy(() => import('./pages/Note/index'));
@@ -35,14 +39,30 @@ export const routes = [
   },
 ];
 
-const App: FC = () => (
-  <Switch>
-    <Suspense fallback={<div>Loading...</div>}>
-      {routes.map(({ path, component }) => (
-        <Route exact path={path} component={component} key={path} />
-      ))}
-    </Suspense>
-  </Switch>
-);
+const App: FC = () => {
+  const { dispatch } = useContext(MainContext);
+
+  useQuery(VERIFY_TOKEN, {
+    onCompleted: ({ me }) => {
+      dispatch({
+        type: TYPES.LOGIN,
+        payload: { user: { username: me.username, email: me.email, avatar: me.avatar } },
+      });
+    },
+    onError: () => {
+      if (localStorage.getItem('token')) localStorage.removeItem('token');
+    },
+  });
+
+  return (
+    <Switch>
+      <Suspense fallback={<div>Loading...</div>}>
+        {routes.map(({ path, component }) => (
+          <Route exact path={path} component={component} key={path} />
+        ))}
+      </Suspense>
+    </Switch>
+  );
+};
 
 export default App;
