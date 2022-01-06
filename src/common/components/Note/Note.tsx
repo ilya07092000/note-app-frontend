fd7b4c77b027e27d4946da9abdc72a58c5bbacfd
ilya07092000 <ischenko2012@gmail.com>
2022-01-06 17:00:37 +0200

    update note component

--- src/common/components/Note/Note.tsx
+++ src/common/components/Note/Note.tsx
@@ -1,8 +1,10 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 import moment from 'moment';
 import { NavLink } from 'react-router-dom';
+import { useToggleFavoriteMutation } from '../../../generated/graphql';
 import FavoritedButton from '../../../containers/FavoritedButton';
 import { IAuthor } from '../../../interfaces/IAuthor';
+import Loader from '../Loader/index';
 
 import styles from './styles.module.scss';
 
@@ -16,22 +18,52 @@ type INoteProps = {
 
 const isLogged = true;
 
-const Note: FC<INoteProps> = ({ favorited = false, author, date, text, id }) => (
-  <NavLink to={`note/${id}`} className={styles.note}>
-    <div className={styles.noteHeader}>
-      <div className={styles.noteInfo}>
-        <img className={styles.noteAvatar} src={author.avatar} alt='avatar' />
-        <div className={styles.noteData}>
-          <p className={styles.author}>{author.username}</p>
-          <p className={styles.date}>{moment(date).format('MMMM Do YYYY')}</p>
+const Note: FC<INoteProps> = ({ favorited = false, author, date, text, id }) => {
+  const [isFavorited, setIsFavorired] = useState(favorited);
+
+  const [toggleFavorite, { loading }]: any = useToggleFavoriteMutation({
+    onCompleted: ({ toggleFavorite }) => {
+      console.log(toggleFavorite);
+      setIsFavorired(() => toggleFavorite?.favorited || false);
+    },
+  });
+
+  const toggleFavoriteNote = (e: any) => {
+    e.preventDefault();
+    toggleFavorite({
+      variables: {
+        id,
+      },
+    });
+  };
+
+  return (
+    <div className={styles.noteWrapper}>
+      <NavLink to={`note/${id}`} className={styles.note}>
+        <div className={styles.noteHeader}>
+          <div className={styles.noteInfo}>
+            <img className={styles.noteAvatar} src={author.avatar} alt='avatar' />
+            <div className={styles.noteData}>
+              <p className={styles.author}>{author.username}</p>
+              <p className={styles.date}>{moment(date).format('MMMM Do YYYY')}</p>
+            </div>
+          </div>
+          {isLogged ? (
+            <FavoritedButton onClick={toggleFavoriteNote} favorited={isFavorited} />
+          ) : null}
         </div>
-      </div>
-      {isLogged ? (
-        <FavoritedButton favorited={favorited} />
+        <div className={styles.noteContent}>{text}</div>
+      </NavLink>
+
+      {loading ? (
+        <div className={styles.loaderWrap}>
+          <div className={styles.loaderInner}>
+            <Loader visible={loading} />
+          </div>
+        </div>
       ) : null}
     </div>
-    <div className={styles.noteContent}>{text}</div>
-  </NavLink>
-);
+  );
+};
 
 export default Note;
