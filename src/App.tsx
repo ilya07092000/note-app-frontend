17eabaaf2656c2c755c4c0f77348ebad0d7c85e0
ilya07092000 <ischenko2012@gmail.com>
2022-01-10 09:39:51 +0200

    feat: use me query, added favorite page

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
 import React, { FC, lazy, Suspense, useContext } from 'react';
-import { useQuery } from '@apollo/client';
 import { Route, Switch } from 'react-router-dom';
+import { useMeQuery } from './generated/graphql';
 import './assets/styles/main.scss';
 import Signup from './pages/Signup/index';
 import Signin from './pages/SignIn/index';
 import withAuth from './common/hoc/withAuth';
-import { VERIFY_TOKEN } from './common/graphql/user/verifyToken';
 import { MainContext } from './common/context/main';
 import { ACTION_TYPES as TYPES } from './interfaces/MainContext/IActionTypes';
 
 const Home = lazy(() => import('./pages/Home/index'));
 const Note = lazy(() => import('./pages/Note/index'));
 const CreateNote = lazy(() => import('./pages/CreateNote/index'));
+const Favorite = lazy(() => import('./pages/Favorite/index'));
 
 export const routes = [
   {
@@ -33,6 +33,12 @@ export const routes = [
     isInSidebar: true,
   },
   {
+    name: 'favorite',
+    path: '/favorite',
+    component: withAuth(Favorite),
+    isInSidebar: true,
+  },
+  {
     name: 'Signup',
     path: '/signup',
     component: Signup,
@@ -49,7 +55,7 @@ export const routes = [
 const App: FC = () => {
   const { dispatch } = useContext(MainContext);
 
-  useQuery(VERIFY_TOKEN, {
+  useMeQuery({
     onCompleted: ({ me }) => {
       dispatch({
         type: TYPES.LOGIN,
